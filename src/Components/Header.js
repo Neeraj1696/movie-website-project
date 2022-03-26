272f16a12a93d7e7d6de1bbd920302a9ce3f72dc
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-03-26 19:55:25 +0530

    second commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header_component">
+      <span className="header">MOVIE WEBSITE FOR WATCH MOVIES FREE </span>
+    </div>
+  );
+};
+
+export default Header;
